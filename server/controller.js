d4c012f6ea5e415e2df80ba95a5ac9b897d0d744
jwbackhouse <jwbackhouse@gmail.com>
2020-10-18 09:18:04 +0000

    Add Express controller placeholder functions.

---
+++ server/controller.js
@@ -0,0 +1,34 @@
+// const { validationResult } = require('express-validator');
+
+// Students
+exports.studentTests_get = (req, res) => {
+  // // Handle errors from express-validator
+  // const errors = validationResult(req);
+  // if (!errors.isEmpty()) {
+  //   return res.status(400).json({ errors: errors.array() });
+  // }
+
+  res.send();
+};
+
+exports.test_get = (req, res) => {
+
+}
+
+exports.test_post = (req, res) => {
+
+}
+
+
+// Teachers
+exports.allTests_get = (req, res) => {
+
+};
+
+exports.setLive_post = (req, res) => {
+
+}
+
+exports.addTest_post = (req, res)=> {
+
+}

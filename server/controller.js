87f73a3eddd72a9e3bc8ec39b8eb84e872b55052
jwbackhouse <jwbackhouse@gmail.com>
2020-10-18 19:45:53 +0000

    Build GET endpoint for live tests.

--- server/controller.js
+++ server/controller.js
@@ -1,14 +1,40 @@
+const db = require('./db.js');
 // const { validationResult } = require('express-validator');
 
 // Students
-exports.studentTests_get = (req, res) => {
-  // // Handle errors from express-validator
-  // const errors = validationResult(req);
-  // if (!errors.isEmpty()) {
-  //   return res.status(400).json({ errors: errors.array() });
-  // }
+exports.studentTests_get = async (req, res) => {
+  try {
+    const studentId = req.params.studentId;
 
-  res.send();
+    let testArr = [];
+
+    // Get live tests only
+    db.collection("tests")
+    .where('live', '==', true)
+    .get()
+    .then((querySnapshot) => {
+      const resultArr = querySnapshot.docs;
+
+      for (let i = 0; i < resultArr.length; i++) {
+        // Pass completed flag alongside full data object
+        const allStudentIds = Object.keys(resultArr[i].data().studentsCompleted);
+        const completed = allStudentIds.includes(studentId);
+
+        testArr.push({
+          [`test${i}`]: resultArr[i].data(),
+          completed,
+        });
+      }
+    res.send(testArr);
+    })
+    .catch(error => {
+      console.log('Error fetching test list from db: ', error);
+      res.status(404).send();
+    });
+  } catch (err) {
+    console.log('Error in studentTests_get():', err);
+    res.status(404).send();
+  }
 };
 
 exports.test_get = (req, res) => {
@@ -16,7 +42,11 @@ exports.test_get = (req, res) => {
 }
 
 exports.test_post = (req, res) => {
-
+  // // Handle errors from express-validator
+  // const errors = validationResult(req);
+  // if (!errors.isEmpty()) {
+  //   return res.status(400).json({ errors: errors.array() });
+  // }
 }
 
 

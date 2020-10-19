f4032b80279c458b1612adfb8e0b3685ce247585
jwbackhouse <jwbackhouse@gmail.com>
2020-10-19 19:05:21 +0000

    Remove redundant code.

--- server/index.js
+++ server/index.js
@@ -1,21 +1,11 @@
-const path = require('path');
 const express = require('express');
 require('dotenv').config();
 const router = require('./router.js');
 
 const port = process.env.PORT || 8080;
 
-// const publicPath = path.join(__dirname, '../client');
-// const viewsPath = path.join(__dirname, '../templates');
-
-// Configure Express
 const app = express();
-// app.set('view engine', 'pug');
-// app.set('views', viewsPath);
-// app.use(express.static(publicPath));
-// app.use(express.urlencoded({
-//   extended: true
-// }));
+
 app.use(express.json());
 // Handle CORS issues
 app.use((req, res, next) => {

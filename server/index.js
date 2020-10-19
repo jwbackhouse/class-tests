740f3a5f081754ade61393ad76d8e7b1ca61afc5
jwbackhouse <jwbackhouse@gmail.com>
2020-10-19 09:16:32 +0000

    Restore deleted dotenv requirement.

--- server/index.js
+++ server/index.js
@@ -1,14 +1,23 @@
-// All variables now available under process.env.<NAME>
-const dotenv = require('dotenv').config();
+const path = require('path');
 const express = require('express');
+require('dotenv').config();
 const router = require('./router.js');
 
 const port = process.env.PORT || 8080;
 
-const app = express();
+// const publicPath = path.join(__dirname, '../client');
+// const viewsPath = path.join(__dirname, '../templates');
 
-// Handle CORS issues
+// Configure Express
+const app = express();
+// app.set('view engine', 'pug');
+// app.set('views', viewsPath);
+// app.use(express.static(publicPath));
+// app.use(express.urlencoded({
+//   extended: true
+// }));
 app.use(express.json());
+// Handle CORS issues
 app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");

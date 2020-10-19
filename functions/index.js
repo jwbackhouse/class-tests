1905de49dc8312a198e25bb62f2d97469458cd91
jwbackhouse <jwbackhouse@gmail.com>
2020-10-19 19:44:22 +0000

    Add score calculation for re-taken tests & refactor code.

--- functions/index.js
+++ functions/index.js
@@ -1,5 +1,6 @@
 // Firebase Cloud Function
 // Calculates score when new answers added to a test
+// Triggered in background by collection updates
 
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
@@ -8,7 +9,30 @@ admin.initializeApp();
 
 const db = admin.firestore();
 
-// Listen for changes in Firestore
+const updateFirestore = (docId, studentId, answers) => {
+  db.collection('tests').doc(docId).get()
+    .then(doc => {
+      const questions = doc.data().questions;
+      let score = 0;
+
+      // Calculate score
+      for (let i = 0; i < answers.length; i++) {
+        if (answers[i] === questions[i].correct) {
+          score += 1;
+        }
+      }
+
+      // Update student's entry
+      doc.ref.update({
+        [`studentsCompleted.${studentId}.score`]: score,
+        })
+        .then(() => console.log('Updated successfully'))
+        .catch(error => console.log('Error updating score:', error));
+    })
+    .catch(error => console.log('Error retrieving questions:', error));
+};
+
+// Listen for changes in studentsCompleted nested object
 exports.calcScore = functions.firestore
   .document('tests/{docId}')
   .onUpdate((change, context) => {
@@ -20,31 +44,11 @@ exports.calcScore = functions.firestore
     const newStudentKeys = Object.keys(newData);
 
     for (let key of newStudentKeys) {
-      // Check if new student answers exist
-      if (!oldStudentKeys.includes(key)) {
-        const answers = newData[key].answers;
-
-        // Get the updated document
-        db.collection('tests').doc(docId).get()
-          .then(doc => {
-            const questions = doc.data().questions;
-            let score = 0;
-
-            // Calculate score
-            for (let i = 0; i < answers.length; i++) {
-              if (answers[i] === questions[i].correct) {
-                score += 1;
-              }
-            }
-
-            // Update student's entry
-            doc.ref.update({
-              [`studentsCompleted.${key}.score`]: score,
-              })
-              .then(() => console.log('Updated successfully'))
-              .catch(error => console.log('Error updating score:', error));
-          })
-          .catch(error => console.log('Error retrieving questions:', error));
+      // Check for new student submission OR new answers
+      if (!oldStudentKeys.includes(key) ||
+        oldData[key].answers != newData[key].answers
+      ) {
+        updateFirestore(docId, key, newData[key].answers);
       }
     }
   });
